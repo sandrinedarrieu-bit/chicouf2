3d081138772ee3bdee058928df6489427bafda79
sandrinedarrieu-bit <sandrine.darrieu@free.fr>
2026-08-16 10:08:19 +0200

    Delete Logout.js

--- Logout.js
+++
@@ -1,5 +0,0 @@
-// api/auth/logout.js
-export default async function handler(req, res) {
-  res.setHeader('Set-Cookie', 'coch_session=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0');
-  return res.status(200).json({ ok: true });
-}
